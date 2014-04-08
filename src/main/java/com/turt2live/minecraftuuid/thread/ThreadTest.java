f6cebe04549789f78138321dedf4894a54d3814e
turt2live <travpc@gmail.com>
2014-04-08 13:50:04 -0600

    Migration move

--- src/main/java/com/turt2live/minecraftuuid/thread/ThreadTest.java
+++ src/main/java/com/turt2live/minecraftuuid/thread/ThreadTest.java
@@ -20,7 +20,7 @@ public class ThreadTest implements Runnable {
 
     public static void main(String[] args) {
         final char[] valid = "abcdefghijklmnopqrstuvwxyz1234567890_".toCharArray();
-        final String last = "b767d";
+        final String last = "gdoss";
 
         for (int i = 0; i < 5; i++) {
             new Thread(new ThreadTest()).start();
