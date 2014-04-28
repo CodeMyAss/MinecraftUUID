f48c045376894c5acbba4bc038e8dfde54d5c328
turt2live <travpc@gmail.com>
2014-04-27 19:32:59 -0600

    fun stuff

--- src/main/java/com/turt2live/minecraftuuid/thread/ThreadTest.java
+++ src/main/java/com/turt2live/minecraftuuid/thread/ThreadTest.java
@@ -1,14 +1,12 @@
 package com.turt2live.minecraftuuid.thread;
 
-import com.turt2live.minecraftuuid.UUIDFetcher;
+import com.turt2live.minecraftuuid.api.UUIDServiceProvider;
 
-import java.io.BufferedWriter;
+import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -17,141 +15,83 @@ public class ThreadTest implements Runnable {
 
     private static ConcurrentLinkedQueue<String> QUEUE = new ConcurrentLinkedQueue<String>();
     private static AtomicBoolean DONE = new AtomicBoolean(false);
+    private static List<String> names = new ArrayList<String>(1000000);
 
     public static void main(String[] args) {
-        final char[] valid = "abcdefghijklmnopqrstuvwxyz1234567890_".toCharArray();
-        final String last = args.length > 0 ? args[0] : "pvp90";
-
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 10; i++) {
             new Thread(new ThreadTest()).start();
         }
 
         new Thread(new Runnable() {
-            boolean didLast = false;
 
             @Override
             public void run() {
-                for (int i = last.length(); i < 17; i++) {
-                    String start = generate(i);
-                    if (!didLast) {
-                        start = last;
-                        didLast = true;
-                    }
-                    QUEUE.add(start);
-                    while ((start = next(start)) != null) {
-                        QUEUE.add(start);
-                        while (QUEUE.size() > 100000) {
-                            try {
-                                Thread.sleep(100);
-                            } catch (InterruptedException e) {
-                            }
-                        }
+                File[] files = new File(System.getProperty("user.dir")).listFiles();
+                if (files != null && files.length > 0) {
+                    try {
+                        scan(files);
+                    } catch (Exception e) {
+                        e.printStackTrace();
                     }
                 }
                 DONE.set(true);
             }
-
-            private String generate(int i) {
-                String s = "";
-                for (int x = 0; x < i; x++) {
-                    s += valid[0];
-                }
-                return s;
-            }
-
-            private String next(String s) {
-                char[] chars = s.toCharArray();
-                boolean made = false;
-                for (int i = chars.length - 1; i >= 0; i--) {
-                    if (chars[i] != valid[valid.length - 1]) {
-                        chars[i] = valid[indexOf(valid, chars[i]) + 1];
-                        made = true;
-                        for (int o = chars.length - 1; o > i; o--) {
-                            chars[o] = valid[0];
-                        }
-                        break;
-                    }
-                }
-                if (!made) return null;
-                return new String(chars);
-            }
-
-            private int indexOf(char[] chars, char c) {
-                for (int i = 0; i < chars.length; i++) {
-                    if (chars[i] == c) {
-                        return i;
-                    }
-                }
-                return -1;
-            }
         }).start();
+    }
+
+    private static void scan(File[] files) throws Exception {
+        if (files != null && files.length > 0) {
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    scan(file.listFiles());
+                } else {
+                    parse(file);
+                }
+            }
+        }
+    }
+
+    private static void parse(File file) throws Exception {
+        if (file.getName().endsWith(".csv")) {
+            System.out.println(file.getAbsolutePath());
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                char[] chars = line.toCharArray();
+                String name = "", uuid = "";
+                boolean onUuid = true;
+                for (int i = 0; i < chars.length; i++) {
+                    char c = chars[i];
+                    if (c != '"') {
+                        if (c == ',') {
+                            onUuid = !onUuid;
+                        } else {
+                            if (onUuid) {
+                                uuid += c;
+                            } else {
+                                name += c;
+                            }
+                        }
+                    }
+                }
+                QUEUE.add(name);
+            }
+            reader.close();
+        }
     }
 
     @Override
     public void run() {
         while (!DONE.get()) {
-            List<String> buffer = new ArrayList<String>();
             while (QUEUE.size() > 0) {
                 String name = QUEUE.poll();
-                buffer.add(name);
-                if (buffer.size() >= UUIDFetcher.MAX_SEARCH - 1) {
-                    try {
-                        BufferedWriter writer = new BufferedWriter(new FileWriter(new File("thread_" + Thread.currentThread().getId() + ".csv"), true));
-                        try {
-                            Map<String, UUID> results = new UUIDFetcher(buffer).call();
-                            for (Map.Entry<String, UUID> entry : results.entrySet()) {
-                                UUID uuid = entry.getValue();
-                                String uid = uuid == null ? "Unknown" : uuid.toString().replace("-", "");
-                                if (uuid != null) {
-                                    writer.write("\"" + uuid.toString().replace("-", "") + "\",\"" + entry.getKey() + "\"");
-                                    writer.newLine();
-                                }
-                                System.out.println("[" + QUEUE.size() + "] " + entry.getKey() + " = " + uid);
-                            }
-                            writer.flush();
-                            writer.close();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                        buffer.clear();
-                    } catch (Exception e) {
-                    }
-                    try {
-                        Thread.sleep(100);
-                    } catch (InterruptedException e) {
-                    }
-                }
-            }
-            if (buffer.size() > 0) {
-                try {
-                    BufferedWriter writer = new BufferedWriter(new FileWriter(new File("thread_" + Thread.currentThread().getId() + ".csv"), true));
-                    try {
-                        Map<String, UUID> results = new UUIDFetcher(buffer).call();
-                        for (Map.Entry<String, UUID> entry : results.entrySet()) {
-                            UUID uuid = entry.getValue();
-                            String uid = uuid == null ? "Unknown" : uuid.toString().replace("-", "");
-                            if (uuid != null) {
-                                writer.write("\"" + uuid.toString().replace("-", "") + "\",\"" + entry.getKey() + "\"");
-                                writer.newLine();
-                            }
-                            System.out.println("[" + QUEUE.size() + "] " + entry.getKey() + " = " + uid);
-                        }
-                        writer.flush();
-                        writer.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    buffer.clear();
-                } catch (Exception e) {
-                    for (String n : buffer) {
-                        QUEUE.add(n);
-                    }
-                }
-                buffer.clear();
+                UUID uuid = UUIDServiceProvider.getUUID(name);
+                System.out.println("[" + QUEUE.size() + "] " + name + " = " + uuid);
             }
             try {
                 Thread.sleep(100);
             } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         }
     }
